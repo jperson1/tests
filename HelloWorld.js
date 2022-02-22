2113fa080d1a5b9caa4f227f1f52e867c19957fd
James <jperson1@umbc.edu>
2022-02-22 11:23:46 -0500

    first commit

---
+++ HelloWorld.js
@@ -0,0 +1,4 @@
+// Run with node or in browser with index.html
+// Prints "Hello World!" to console
+
+console.log("Hello world!")
---
+++ HelloWorldFancy.js
@@ -0,0 +1,4 @@
+// Run in browser with index.html
+// Prints to console "Hello World!" in color
+
+console.log('%c Hello World! ', 'background: black; color: green;')

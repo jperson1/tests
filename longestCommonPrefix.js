23a0de3502ce4290982ad36ad35e7720e96115a8
James <jperson1@umbc.edu>
2022-02-28 01:24:06 -0500

    longest common prefix

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -5,12 +5,21 @@
 
 // Given an array of strings, return the longest common prefix. If none, return the empty string.
 var longestCommonPrefix = function (strs) {
+    let c = '', p = '';
 
+    for (let i=0; i<strs[0].length; i++){
+        c = strs[0].charAt(i);
+        for (let j=1; j<strs.length; j++){
+            if (strs[j].charAt(i) != c){
+                return p;
+            }
+        }
+        p = p + c;
+    }
 
-
-    return strs;
+    return p;
 };
 
 console.log(longestCommonPrefix(["flower", "flow", "flight"]));
 console.log(longestCommonPrefix(["dog", "racecar", "car"]));
-console.log(longestCommonPrefix(["pig, piglet, piggy"]));
+console.log(longestCommonPrefix(["pig", "piglet", "piggy"]));

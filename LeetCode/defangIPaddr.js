3ee5a1e90db369b7c08758ef66141b9023be5c90
James <jperson1@umbc.edu>
2022-03-17 12:26:18 -0400

    several problems

---
+++ LeetCode/defangIPaddr.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} address
+ * @return {string}
+ */
+
+//  Given a valid (IPv4) IP address, return a defanged version of that IP address.
+//  A defanged IP address replaces every period "." with "[.]".
+var defangIPaddr = function (address) {
+    return address.split('.').join("[.]");
+};
+
+console.log(defangIPaddr("1.1.1.1"));
+console.log(defangIPaddr("255.100.50.0"));
